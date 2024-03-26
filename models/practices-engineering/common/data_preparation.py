1f7118fd1ecbb17f06fbae44ebd156eea2098237
Alex Serna <asernabo@redhat.com>
2024-03-26 09:55:20 +0100

    Update models/practices-engineering/common/data_preparation.py
    Co-authored-by: Jaime Ramírez <jaime.ramirez@redhat.com>

--- models/practices-engineering/common/data_preparation.py
+++ models/practices-engineering/common/data_preparation.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 def prepare_training_data(
         csv_path: Path, test_size: float = 0.2, random_state: int = 0
 ) -> List:
-    path = Path(csv_path) if isinstance(csv_path, str) else csv_path
+    path = Path(csv_path)
     data = pd.read_csv(path.as_posix())
 
     x = data.drop("Outcome", axis=1)
