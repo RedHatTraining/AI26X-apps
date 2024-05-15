d7996a9c88c8a03a7dee5c9e664fc8a6b1cc684c
Pablo Solar Vilariño <psolarvi@redhat.com>
2024-05-15 10:50:50 +0200

    Transform final code into a template

--- pipelines/kubeflow-pipeline/kubeflow-pipeline.py
+++ pipelines/kubeflow-pipeline/kubeflow-pipeline.py
@@ -15,16 +15,16 @@
 from kfp import components
 from kfp import dsl
 
-import random
-
 
 def random_num(low: int, high: int) -> int:
     """Generate a random number between low and high."""
+    import random
     return random.randint(low, high)
 
 
 def flip_coin() -> str:
     """Flip a coin and output heads or tails randomly."""
+    import random
     return 'heads' if random.randint(0, 1) == 0 else 'tails'
 
 
@@ -34,33 +34,13 @@ def print_msg(msg: str):
 
 
 # TODO: Create a component from the flip_coin function
-flip_coin_op = components.create_component_from_func(
-    flip_coin, base_image='registry.access.redhat.com/ubi9/python-39')
 
 # TODO: Create a component from the print_msg function
-print_op = components.create_component_from_func(
-    print_msg, base_image='registry.access.redhat.com/ubi9/python-39')
 
 # TODO: Create a component from the random_num function
-random_num_op = components.create_component_from_func(
-    random_num, base_image='registry.access.redhat.com/ubi9/python-39')
 
 
 # TODO: Define a pipeline
-@dsl.pipeline(
-    name='coin-toss-pipeline',
-    description='A simple pipeline'
-)
 def flipcoin_pipeline():
     flip = flip_coin_op()
     # TODO: Add pipeline logic
-    with dsl.Condition(flip.output == 'heads', 'Heads result'):
-        print_op('Heads!')
-        random_number = random_num_op(0, 9)
-        with dsl.Condition(random_number.output > 7):
-            print_op('A high value!')
-        with dsl.Condition(random_number.output <= 7):
-            print_op('A low value!')
-
-    with dsl.Condition(flip.output == 'tails', 'Tails result'):
-        print_op('Tails result')
