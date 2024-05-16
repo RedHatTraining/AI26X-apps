73e7b782dae8b01bffccfd062c23ba38ecd2a2da
Alex Serna <asernabo@redhat.com>
2024-05-16 16:46:56 +0200

    format scritp

--- deploying/rhoaiserving-custom/build_and_push.sh
+++ deploying/rhoaiserving-custom/build_and_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
-podman login -u=developer -p=developer registry.ocp4.example.com:8443
-podman build . -t  registry.ocp4.example.com:8443/developer/sklearn-model-server
-podman push registry.ocp4.example.com:8443/developer/sklearn-model-server
+QUAY_HOST="registry.ocp4.example.com:8443"
+podman login -u=developer -p=developer ${QUAY_HOST}
+podman build . -t  ${QUAY_HOST}/developer/sklearn-model-server
+podman push ${QUAY_HOST}/developer/sklearn-model-server
