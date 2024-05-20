b4e5c1640a9419038515ae9117c7aba4ccaa0efd
Alex Serna <asernabo@redhat.com>
2024-05-20 18:16:11 +0200

    remove unnecesary code

--- deploying/rhoaiserving-custom/solution/model.py
+++ deploying/rhoaiserving-custom/solution/model.py
@@ -1,16 +1,3 @@
-# Copyright 2021 The KServe Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 import argparse
 import os
 import pathlib
@@ -63,7 +50,7 @@ class SKLearnModel(Model):
 
 parser = argparse.ArgumentParser(parents=[model_server.parser])
 parser.add_argument(
-    "--model_dir", required=True, help="A local path to the model binary"
+    "--model_dir", required=True, help="A local path to the model file"
 )
 args, _ = parser.parse_known_args()
 
