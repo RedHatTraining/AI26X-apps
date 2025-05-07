4825a4b6c4e065cd7a3d4063a2e409fc11b2a04b
Jaime Ramirez <jaime.ramirez@redhat.com>
2025-05-07 11:00:30 +0200

    Fix other code problems

--- deploying/rhoaiserving-using/utils.py
+++ deploying/rhoaiserving-using/utils.py
@@ -59,14 +59,12 @@ def send_inference_request(url, body, token=None):
     return requests.post(url, json=body, headers=headers, verify=False)
 
 
-def print_curl_request(url, tokens):
-    # Input text for sentiment analysis
-    text = "This model is great!"
-
+def print_curl_request(url, query):
     print(
         f'\n{BOLD}{GREEN}Inferece request for the {url} url, using "{query}" as input.{RESET}\n'
     )
     # Tokenize the input text
+    tokens = tokenize(query)
 
     # Define request and print
     body = f"""'{{"instances": [
@@ -83,4 +81,4 @@ def print_curl_request(url, tokens):
 if __name__ == "__main__":
     query = sys.argv[2]
     url = sys.argv[1]
-    gen_request(url, query)
+    print_curl_request(url, query)
