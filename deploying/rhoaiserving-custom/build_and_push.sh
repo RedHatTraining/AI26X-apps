cf79b0ecf3a9a1a7bc1dd1e17a82f13f63e4cb9e
Alex Serna <asernabo@redhat.com>
2025-11-12 15:03:00 +0100

    clean exercises in preparation for first lesson release

--- deploying/rhoaiserving-custom/build_and_push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-QUAY_HOST="registry.ocp4.example.com:8443"
-podman login -u=developer -p=developer ${QUAY_HOST}
-podman build . -t  ${QUAY_HOST}/developer/sklearn-model-server
-podman push ${QUAY_HOST}/developer/sklearn-model-server
